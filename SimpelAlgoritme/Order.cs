89ada92dcc255917a5763caef8a8d1b6c941c8fd
abevdwerf <479395@student.fontys.nl>
2022-03-14 12:57:28 +0100

    Add project files.

---
+++ SimpelAlgoritme/Order.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpelAlgoritme
+{
+    internal class Order
+    {
+        private Double GiveMaximumPrice()
+        {
+
+        }
+
+        private Double GiveAveragePrice()
+        {
+
+        }
+
+        private List<Product> GetAllProducts(Double minimumPrice)
+        {
+
+        }
+
+        private void SortProductByPrice()
+        {
+
+        }
+    }
+}
---
+++ SimpelAlgoritme/Product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpelAlgoritme
+{
+    internal class Product
+    {
+        public string Name { get; private set; }
+        public Double Price { get; private set; }
+
+        Product(string name, Double price)
+        {
+            this.Name = name;
+            this.Price = price;
+        }
+    }
+}
