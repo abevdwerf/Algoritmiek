50eaf522d40fc6938eb5eb4b0b325947c1fb745a
abevdwerf <479395@student.fontys.nl>
2022-03-26 13:12:04 +0100

    add new project "circustrein"

---
+++ Circustrein/Animal.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Circustrein
+{
+    internal class Animal
+    {
+        enum Type
+        {
+            Carnivore,
+            Herbivore
+        }
+
+        enum Size
+        {
+            Small = 1,
+            Medium = 3,
+            Large = 5
+        }
+
+        private Type type;
+        private Size size;
+
+        public Animal(Type type, Size size)
+        {
+            this.type = type;
+            this.size = size;
+        }
+    }
+}
