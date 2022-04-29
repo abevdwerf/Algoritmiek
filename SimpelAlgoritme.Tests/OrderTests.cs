f59f1ae64b95d1db14be0c616acd7faae915b050
abevdwerf <479395@student.fontys.nl>
2022-04-29 10:41:46 +0200

    first unittest

---
+++ SimpelAlgoritme.Tests/OrderTests.cs
@@ -0,0 +1,18 @@
+using Xunit;
+
+namespace SimpelAlgoritme.Tests
+{
+    public class OrderTests
+    {
+        [Fact]
+        public void Order_GiveMaximumPrice()
+        {
+            //Arrange
+            //Order order = new Order();
+
+            //Action
+
+            //Assert
+        }
+    }
+}
